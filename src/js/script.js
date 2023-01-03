f6685e5bd49fa0ae8e6099fe3aa58b9666e70151
Mateusz Myszke <myszke93@gmail.com>
2023-01-03 15:39:51 +0100

    Update initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -73,17 +73,20 @@
     initAccordion(){
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = ???;
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
       /* prevent default action for event */
-
-      /* find active product (product that has active class) */
-
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-
-      /* toggle active class on thisProduct.element */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(((activeProduct != null) && activeProduct.contains('active')) && (activeProduct != thisProduct.element)) {
+          activeProduct.classList.remove('active');
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
       });
 
     }
