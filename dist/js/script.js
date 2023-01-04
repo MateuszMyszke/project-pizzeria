094e9e190ce10e24073d8b2c578bd7002e6f595d
Mateusz Myszke <myszke93@gmail.com>
2023-01-04 14:15:46 +0100

    Add images to loop for processOrder

--- dist/js/script.js
+++ dist/js/script.js
@@ -83,6 +83,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -126,38 +127,33 @@
 
     processOrder(){
       const thisProduct = this;
-      // convert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      
       const formData = utils.serializeFormToObject(thisProduct.form);
-      // set price to default price
+      
       let price = thisProduct.data.price;
-      // for every category (param)...
       for(let paramId in thisProduct.data.params) {
-        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+       
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
-
-        // for every option in this category
         for(let optionId in param.options) {
-          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          const selected = formData[paramId].includes(optionId);
-          // console.log(optionId, option);
-          // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && selected) {
-            // check if the option is not default
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected) {
             if(!option.default) {
-              // add option price to price variable
-              console.log('opt1');
               price += option.price;
             }
           } else if (option.default) {
-            // check if the option is default
-            // reduce price variable
-            console.log('opt2');
             price -= option.price;
           }
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
+            }
+          }
         }
-        // update calculated price in the HTML
+        
         thisProduct.priceElem.innerHTML = price;
       }
     }
