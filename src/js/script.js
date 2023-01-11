9cb87b6a38224cb6a6f4270aab33a079eecf1d83
Mateusz Myszke <myszke93@gmail.com>
2023-01-11 16:44:15 +0100

    Add listener for class Cart in method initActions

--- src/js/script.js
+++ src/js/script.js
@@ -273,7 +273,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
 
@@ -322,6 +324,9 @@
       const thisCart = this;
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
