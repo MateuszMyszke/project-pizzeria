665459f8d5a862d46da45f0283b2b31c19ab025c
Mateusz Myszke <myszke93@gmail.com>
2023-01-21 18:28:18 +0100

    Add class Booking

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,53 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navlinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let  pageMatchingHash = false;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.actiavePage(pageMatchingHash);
+
+    for(let link of thisApp.navlinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.actiavePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  actiavePage: function(pageId){
+    const thisApp = this;
+
+    /*add class 'active' to maching pages, remove from non-matching*/
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /*add class 'active' to maching pages, remove from non-matching*/
+
+    for(let link of thisApp.navlinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
     for(let productData in thisApp.data.products){
@@ -36,11 +81,22 @@ const app = {
     });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+    const bookElem = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookElem);
+
+  },
+
   init: function(){
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
+
+  
 };
 
 app.init();
