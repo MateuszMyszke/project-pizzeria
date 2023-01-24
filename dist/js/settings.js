0702e9f0146f4b76d457c6bd53ac5693ed2823de
Mateusz Myszke <myszke93@gmail.com>
2023-01-24 23:52:30 +0100

    Add sendBooking and send reservation to API

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -43,11 +43,17 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    floor: '.floor-plan',
+    duration: '[name="hours"]',
+    people: '[name="people"]',
+    submit: '.booking-form [type="submit"]',
+    address: '[name="address"]',
+    phone: '[name="phone"]',
+    starters: 'input[type="checkbox"]',
   },
   nav: {
     links: '.main-nav a',
   },
-  
   cart: {
     productList: '.cart__order-summary',
     toggleTrigger: '.cart__summary',
@@ -67,19 +73,19 @@ export const select = {
     remove: '[href="#remove"]',
   },
 };
-
+  
 export const classNames = {
   menuProduct: {
     wrapperActive: 'active',
     imageVisible: 'active',
   },
- 
   cart: {
     wrapperActive: 'active',
   },
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
@@ -88,13 +94,13 @@ export const classNames = {
     active: 'active',
   }
 };
-
+  
 export const settings = {
   amountWidget: {
     defaultValue: 1,
-    defaultMin: 0,
-    defaultMax: 10,
-  },
+    defaultMin: 1,
+    defaultMax: 9,
+  }, 
   cart: {
     defaultDeliveryFee: 20,
   },
@@ -120,10 +126,9 @@ export const settings = {
     repeatParam: 'repeat_ne=false',
   },
 };
-
+  
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
-};  
+};
