2f8c98da1c15546a44197528fbf2ff41d6bd4860
Mateusz Myszke <myszke93@gmail.com>
2023-01-24 21:08:45 +0100

    Add getData

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,4 +1,5 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -8,8 +9,62 @@ class Booking{
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
 
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsReapeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+      
+    };
+    
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking 
+                               + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.events 
+                               + '?' + params.eventsCurrent.join('&'),
+      eventsReapeat:  settings.db.url + '/' + settings.db.events 
+                               + '?' + params.eventsCurrent.join('&'),
+    };
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsReapeat),
+    ])
+    
+      .then(function(allResponse){
+        const bookingsResponse = allResponse[0];
+        const eventsCurrentResponse = allResponse[1];
+        const eventsReapeatResponse = allResponse[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsReapeatResponse.json(),
+        ]);
+      })
+      .then(function(bookings, eventsCurrent , eventsReapeat){
+        console.log(bookings);
+      });
+
+  }
   render(element){
     const thisBooking = this;
     const generatedHTML = templates.bookingWidget();
